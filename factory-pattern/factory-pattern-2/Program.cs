5dfe82e26d8edd35b29cea37349cdd99d4ca3bb3
shayan vosoughi <shayanwqhw@gmail.com>
2024-04-23 13:25:01 +0330

    add factory pattern

---
+++ factory-pattern/factory-pattern-2/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace factory_pattern_2
+{
+    internal class Program
+    {
+        interface ITransport
+        {
+            string Operation();
+        }
+
+        abstract class Logistics
+        {
+            abstract public ITransport planDelivery();
+            public string createTransport() {
+                ITransport transport = planDelivery();
+                string text = $"transport created: {transport.Operation()}";
+                return text;
+            }
+        }
+
+        class Truck: ITransport {
+            public string Operation() => "this is truck";
+        }
+
+        class Ship: ITransport
+        {
+            public string Operation() => "this is ship";
+        }
+
+        class RoadLogistics: Logistics
+        {
+            public override ITransport planDelivery()
+            {
+                return new Truck();
+            }
+        }
+
+        class SeaLogistics: Logistics
+        {
+            public override ITransport planDelivery()
+            {
+                return new Ship();
+            }
+        }
+
+        class Client()
+        {
+            public void Main()
+            {
+                Console.WriteLine("try to deliver road logisitc...");
+                ClientCode(new RoadLogistics());
+
+                Console.WriteLine("");
+
+                Console.WriteLine("try to deliver sea logisitc...");
+                ClientCode(new SeaLogistics());
+            }
+
+            public void ClientCode(Logistics logistics)
+            {
+                Console.WriteLine($"{logistics.createTransport()}");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            new Client().Main();
+            Console.ReadKey();
+        }
+    }
+}
